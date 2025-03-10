b472b097d4cb119e13b4f7ec1e95a0a35c61dc86
Hein Bekker <hein@netbek.co.za>
2025-03-10 19:28:55 +0200

    Add link to docs

--- src/dw/types.py
+++ src/dw/types.py
@@ -83,6 +83,7 @@ def calculate_threads(percent) -> int:
     return int(math.floor(psutil.cpu_count(logical=True) * percent / 100))
 
 
+# Default values from https://duckdb.org/docs/stable/configuration/overview.html#global-configuration-options
 class DuckDBSystemSettings(BaseModel):
     model_config = ConfigDict(extra="allow")
 

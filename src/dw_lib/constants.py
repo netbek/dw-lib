e57a10ca823a26f532db6dc5159c68d12f411336
Hein Bekker <hein@netbek.co.za>
2025-12-22 07:27:31 +0200

    Move dbt constants to module

--- src/dw_lib/constants.py
+++ src/dw_lib/constants.py
@@ -2,64 +2,6 @@
 PEERDB_SOURCE_PEER = "source"
 PEERDB_DESTINATION_PEER = "destination"
 
-"""
-Map from dbt-codegen to ClickHouse data types that are case-sensitive.
-See https://clickhouse.com/docs/en/operations/system-tables/data_type_families
-Query:
-    select '"' || lower(name) || '": "' || name || '",'
-    from system.data_type_families
-    where not case_insensitive
-    order by name;
-"""
-CODEGEN_TO_CLICKHOUSE_DATA_TYPE = {
-    "aggregatefunction": "AggregateFunction",
-    "array": "Array",
-    "enum16": "Enum16",
-    "enum8": "Enum8",
-    "fixedstring": "FixedString",
-    "float32": "Float32",
-    "float64": "Float64",
-    "ipv4": "IPv4",
-    "ipv6": "IPv6",
-    "int128": "Int128",
-    "int16": "Int16",
-    "int256": "Int256",
-    "int32": "Int32",
-    "int64": "Int64",
-    "int8": "Int8",
-    "intervalday": "IntervalDay",
-    "intervalhour": "IntervalHour",
-    "intervalmicrosecond": "IntervalMicrosecond",
-    "intervalmillisecond": "IntervalMillisecond",
-    "intervalminute": "IntervalMinute",
-    "intervalmonth": "IntervalMonth",
-    "intervalnanosecond": "IntervalNanosecond",
-    "intervalquarter": "IntervalQuarter",
-    "intervalsecond": "IntervalSecond",
-    "intervalweek": "IntervalWeek",
-    "intervalyear": "IntervalYear",
-    "lowcardinality": "LowCardinality",
-    "map": "Map",
-    "multipolygon": "MultiPolygon",
-    "nested": "Nested",
-    "nothing": "Nothing",
-    "nullable": "Nullable",
-    "object": "Object",
-    "point": "Point",
-    "polygon": "Polygon",
-    "ring": "Ring",
-    "simpleaggregatefunction": "SimpleAggregateFunction",
-    "string": "String",
-    "tuple": "Tuple",
-    "uint128": "UInt128",
-    "uint16": "UInt16",
-    "uint256": "UInt256",
-    "uint32": "UInt32",
-    "uint64": "UInt64",
-    "uint8": "UInt8",
-    "uuid": "UUID",
-    "variant": "Variant",
-}
 
 PYTHON_KEYWORDS = [
     "false",
--- src/dw_lib/dbt/__init__.py
+++ src/dw_lib/dbt/__init__.py
@@ -1,4 +1,3 @@
-from ..constants import CODEGEN_TO_CLICKHOUSE_DATA_TYPE
 from ..utils.filesystem import find_up, get_file_extension
 from ..utils.yaml_utils import safe_load_file
 from .types import DbtModel, DbtResourceType, DbtSeed, DbtSource
@@ -20,6 +19,65 @@ RESOURCE_TYPE_TO_CLASS = {
     DbtResourceType.MODEL: DbtModel,
     DbtResourceType.SEED: DbtSeed,
     DbtResourceType.SOURCE: DbtSource,
+}
+
+"""
+Map from dbt-codegen to ClickHouse data types that are case-sensitive.
+See https://clickhouse.com/docs/en/operations/system-tables/data_type_families
+Query:
+    select '"' || lower(name) || '": "' || name || '",'
+    from system.data_type_families
+    where not case_insensitive
+    order by name;
+"""
+CODEGEN_TO_CLICKHOUSE_DATA_TYPE = {
+    "aggregatefunction": "AggregateFunction",
+    "array": "Array",
+    "enum16": "Enum16",
+    "enum8": "Enum8",
+    "fixedstring": "FixedString",
+    "float32": "Float32",
+    "float64": "Float64",
+    "ipv4": "IPv4",
+    "ipv6": "IPv6",
+    "int128": "Int128",
+    "int16": "Int16",
+    "int256": "Int256",
+    "int32": "Int32",
+    "int64": "Int64",
+    "int8": "Int8",
+    "intervalday": "IntervalDay",
+    "intervalhour": "IntervalHour",
+    "intervalmicrosecond": "IntervalMicrosecond",
+    "intervalmillisecond": "IntervalMillisecond",
+    "intervalminute": "IntervalMinute",
+    "intervalmonth": "IntervalMonth",
+    "intervalnanosecond": "IntervalNanosecond",
+    "intervalquarter": "IntervalQuarter",
+    "intervalsecond": "IntervalSecond",
+    "intervalweek": "IntervalWeek",
+    "intervalyear": "IntervalYear",
+    "lowcardinality": "LowCardinality",
+    "map": "Map",
+    "multipolygon": "MultiPolygon",
+    "nested": "Nested",
+    "nothing": "Nothing",
+    "nullable": "Nullable",
+    "object": "Object",
+    "point": "Point",
+    "polygon": "Polygon",
+    "ring": "Ring",
+    "simpleaggregatefunction": "SimpleAggregateFunction",
+    "string": "String",
+    "tuple": "Tuple",
+    "uint128": "UInt128",
+    "uint16": "UInt16",
+    "uint256": "UInt256",
+    "uint32": "UInt32",
+    "uint64": "UInt64",
+    "uint8": "UInt8",
+    "uuid": "UUID",
+    "variant": "Variant",
 }
 
 

8f872d0c00c566afbca7255e295827306aafc06c
Hein Bekker <hein@netbek.co.za>
2025-12-23 21:49:32 +0200

    Add tests for relation types

---
+++ tests/test_types.py
@@ -0,0 +1,62 @@
+from dw_lib.types import ClickHouseTableIdentifier, PostgresTableIdentifier
+from pydantic_core import ValidationError
+
+import pytest
+
+
+class TestClickHouseTableIdentifier:
+    def test_init_without_table(self):
+        with pytest.raises(ValidationError):
+            ClickHouseTableIdentifier()
+
+    def test_from_string_database_and_table(self):
+        relation = ClickHouseTableIdentifier.from_string("my_database.my_table")
+        assert relation.database == "my_database"
+        assert relation.table == "my_table"
+
+    def test_from_string_table(self):
+        relation = ClickHouseTableIdentifier.from_string("my_table")
+        assert relation.database is None
+        assert relation.table == "my_table"
+
+    # def test_from_string_empty_string(self):
+    #     relation = ClickHouseTableIdentifier.from_string("")
+    #     assert relation.database is None
+    #     assert relation.table is None
+
+    def test_to_string_database_and_table(self):
+        relation = ClickHouseTableIdentifier(database="my_database", table="my_table")
+        assert str(relation) == "`my_database`.`my_table`"
+
+    def test_to_string_table(self):
+        relation = ClickHouseTableIdentifier(table="my_table")
+        assert str(relation) == "`my_table`"
+
+
+class TestPostgresTableIdentifier:
+    def test_init_without_table(self):
+        with pytest.raises(ValidationError):
+            PostgresTableIdentifier()
+
+    def test_from_string_schema_and_table(self):
+        relation = PostgresTableIdentifier.from_string("my_schema.my_table")
+        assert relation.schema_ == "my_schema"
+        assert relation.table == "my_table"
+
+    def test_from_string_table(self):
+        relation = PostgresTableIdentifier.from_string("my_table")
+        assert relation.schema_ is None
+        assert relation.table == "my_table"
+
+    # def test_from_string_empty_string(self):
+    #     relation = PostgresTableIdentifier.from_string("")
+    #     assert relation.schema_ is None
+    #     assert relation.table is None
+
+    def test_to_string_schema_and_table(self):
+        relation = PostgresTableIdentifier(schema_="my_schema", table="my_table")
+        assert str(relation) == '"my_schema"."my_table"'
+
+    def test_to_string_table(self):
+        relation = PostgresTableIdentifier(table="my_table")
+        assert str(relation) == '"my_table"'

2c8fdac13ecbf73c1ecceb534607490cb97265c1
Hein Bekker <hein@netbek.co.za>
2025-12-23 21:59:47 +0200

    Add test for fully qualified name of Postgres relation

--- tests/test_types.py
+++ tests/test_types.py
@@ -37,6 +37,12 @@ class TestPostgresTableIdentifier:
         with pytest.raises(ValidationError):
             PostgresTableIdentifier()
 
+    def test_from_string_database_and_schema_and_table(self):
+        relation = PostgresTableIdentifier.from_string("my_database.my_schema.my_table")
+        assert relation.database == "my_database"
+        assert relation.schema_ == "my_schema"
+        assert relation.table == "my_table"
+
     def test_from_string_schema_and_table(self):
         relation = PostgresTableIdentifier.from_string("my_schema.my_table")
         assert relation.schema_ == "my_schema"
@@ -50,6 +56,12 @@ class TestPostgresTableIdentifier:
     def test_from_string_empty_string(self):
         with pytest.raises(ValueError):
             PostgresTableIdentifier.from_string("")
+
+    def test_to_string_database_and_schema_and_table(self):
+        relation = PostgresTableIdentifier(
+            database="my_database", schema_="my_schema", table="my_table"
+        )
+        assert str(relation) == '"my_database"."my_schema"."my_table"'
 
     def test_to_string_schema_and_table(self):
         relation = PostgresTableIdentifier(schema_="my_schema", table="my_table")

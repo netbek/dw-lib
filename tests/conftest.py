5bff80397b297c90aa93ff1e457903b4569b3e9c
Hein Bekker <hein@netbek.co.za>
2026-04-17 02:43:31 +0200

    chore: Bump Temporal

--- tests/conftest.py
+++ tests/conftest.py
@@ -26,9 +26,9 @@ PEERDB_TEST_ENV = {
     "PEERDB_SERVER_IMAGE": "ghcr.io/peerdb-io/peerdb-server:stable-v0.36.16",
     "PEERDB_UI_IMAGE": "ghcr.io/peerdb-io/peerdb-ui:stable-v0.36.16",
     "POSTGRES_IMAGE": "postgres:18.3-alpine3.23",
-    "TEMPORAL_ADMIN_TOOLS_IMAGE": "temporalio/admin-tools:1.29.4.1-tctl-v1.18.4-cli-v1.6.1",
-    "TEMPORAL_SERVER_IMAGE": "temporalio/server:1.29.4.1",
-    "TEMPORAL_UI_IMAGE": "temporalio/ui:2.48.1",
+    "TEMPORAL_ADMIN_TOOLS_IMAGE": "temporalio/admin-tools:1.29.6-tctl-1.18.4-cli-1.6.2",
+    "TEMPORAL_SERVER_IMAGE": "temporalio/server:1.29.6",
+    "TEMPORAL_UI_IMAGE": "temporalio/ui:2.48.3",
 }
 
 
